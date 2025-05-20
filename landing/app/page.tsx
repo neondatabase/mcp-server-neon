a43b6eb8330a43622967d0e1b4c14f8ed8279f21
Ilia Lebedev <ilia@neon.tech>
2025-05-20 15:34:04 +0200

    extract header

--- landing/app/page.tsx
+++ landing/app/page.tsx
@@ -1,5 +1,4 @@
 import fs from 'fs/promises';
-import Image from 'next/image';
 
 import {
   Accordion,
@@ -7,12 +6,10 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from '@/components/ui/accordion';
-import { Button } from '@/components/ui/button';
-
-import githubSvg from './github.svg';
-import { DescriptionItem, parseDescription } from '@/lib/description';
+import { parseDescription } from '@/lib/description';
 import { DescriptionItemsUi } from '@/components/DescriptionItem';
 import { Introduction } from '@/components/Introduction';
+import { Header } from '@/components/Header';
 
 type ToolDescription = {
   name: string;
@@ -20,60 +17,22 @@ type ToolDescription = {
 };
 
 export default async function Home() {
-  let packageVersion: number | undefined;
-  let tools:
-    | {
-        name: string;
-        description: DescriptionItem[];
-      }[]
-    | undefined;
+  const packageJson = await fs.readFile('../package.json', 'utf-8');
+  const packageVersion = JSON.parse(packageJson).version as number;
 
-  try {
-    const packageJson = await fs.readFile('../package.json', 'utf-8');
-    packageVersion = JSON.parse(packageJson).version;
+  const toolsJson = await fs.readFile('./tools.json', 'utf-8');
+  const rawTools = JSON.parse(toolsJson) as ToolDescription[];
 
-    const toolsJson = await fs.readFile('./tools.json', 'utf-8');
-    const rawTools = JSON.parse(toolsJson) as ToolDescription[];
-
-    tools = rawTools.map(({ description, ...data }) => ({
-      ...data,
-      description: parseDescription(description),
-    }));
-  } catch (error) {
-    console.error(error);
-  }
+  const tools = rawTools.map(({ description, ...data }) => ({
+    ...data,
+    description: parseDescription(description),
+  }));
 
   return (
     <div className="flex flex-col items-center min-h-screen p-4 pb-0 sm:p-8 sm:pb-0">
       <main className="w-full max-w-3xl">
         <article className="flex flex-col gap-10">
-          <header className="flex items-center justify-between gap-2">
-            <div className="flex items-center gap-3">
-              <Image src="/neon.svg" width={30} height={30} alt="Neon logo" />
-              <div className="flex items-baseline gap-2">
-                <h1 className="text-3xl font-bold whitespace-nowrap">
-                  Neon MCP
-                </h1>{' '}
-                version: {packageVersion}
-              </div>
-            </div>
-            <Button asChild>
-              <a
-                href="https://github.com/neondatabase-labs/mcp-server-neon?tab=readme-ov-file"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Image
-                  alt=""
-                  src={githubSvg}
-                  className="invert dark:invert-0"
-                  width={16}
-                  height={16}
-                />{' '}
-                Github
-              </a>
-            </Button>
-          </header>
+          <Header packageVersion={packageVersion} />
           <Introduction />
           <section id="tools">
             <h2 className="text-2xl font-bold mb-2 border-b-3 border-b-emerald-600">
