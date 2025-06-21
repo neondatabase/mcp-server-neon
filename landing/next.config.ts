3cd8ba6e9d6cb1d7bedc69edabc31f55e8daf276
David Gomes <davidrafagomes@gmail.com>
2025-06-21 11:55:38 +0200

    serverComponentsExternalPackages

--- landing/next.config.ts
+++ landing/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
+  experimental: {
+    serverComponentsExternalPackages: ["@tailwindcss/oxide"],
+  },
 };
 
 export default nextConfig;
