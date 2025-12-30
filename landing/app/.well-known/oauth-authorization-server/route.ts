eac6462ce4e0b8e330e2c68490cec04c8f8ed7b8
Pedro Figueiredo <klisarkk@gmail.com>
2025-12-30 11:08:35 +0000

    feat: add revoke method

--- landing/app/.well-known/oauth-authorization-server/route.ts
+++ landing/app/.well-known/oauth-authorization-server/route.ts
@@ -16,11 +16,13 @@ export async function GET() {
     authorization_endpoint: `${SERVER_HOST}/api/authorize`,
     token_endpoint: `${SERVER_HOST}/api/token`,
     registration_endpoint: `${SERVER_HOST}/api/register`,
+    revocation_endpoint: `${SERVER_HOST}/api/revoke`,
     response_types_supported: SUPPORTED_RESPONSE_TYPES,
     response_modes_supported: ['query'],
     grant_types_supported: SUPPORTED_GRANT_TYPES,
     token_endpoint_auth_methods_supported: SUPPORTED_AUTH_METHODS,
     registration_endpoint_auth_methods_supported: SUPPORTED_AUTH_METHODS,
+    revocation_endpoint_auth_methods_supported: SUPPORTED_AUTH_METHODS,
     code_challenge_methods_supported: SUPPORTED_CODE_CHALLENGE_METHODS,
   });
 }
